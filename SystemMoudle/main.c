102c420ad13e9f4e981c81dba49c4f277cbbe54a
chen <chenjunyu_cqupt@outlook.com>
2023-03-06 13:51:07 +0800

    Initial Commit

---
+++ SystemMoudle/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SystemMoudle
+//
+//  Created by chen on 2023/3/6.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
